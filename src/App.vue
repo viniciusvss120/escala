23f58f4d4ca81bbf87f43f22cd1083b143c367fa
Vinicius Silva Vss120 <92170937+viniciusvss120@users.noreply.github.com>
2021-10-25 18:00:11 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="rebeccapurple"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+        
+        />
+         <!-- <img src="img/logo-vertical.png" alt="logo"> -->
+        <v-img
+          
+        />
+      </div>
+
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    
+  }),
+};
+</script>
